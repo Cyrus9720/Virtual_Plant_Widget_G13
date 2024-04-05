1af37950c6c0a6ae2e4ec733b20541eb9c6db83a
Cyrus9720 <cyrusshaerpour@gmail.com>
2024-04-05 10:12:59 +0200

    Update Main.java

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         Controller controller = new Controller();
-        System.out.println("Hello World!");
+        System.out.println("Hello hej!");
         //test
     }
 }
