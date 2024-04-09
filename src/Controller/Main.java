b456cb5d4807c1677f4a916aa2674b07140ea6de
annagranberg <annagranberg03@gmail.com>
2024-04-09 11:19:16 +0200

    tog bort överflödig kod

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -5,7 +5,5 @@ public class Main
     public static void main(String[] args)
     {
         Controller controller = new Controller();
-        System.out.println("Hello hej!");
-        //test
     }
 }
