5585c1387b1a90961f7f3129e68fddb1a865ac4b
annagranberg <annagranberg03@gmail.com>
2024-05-17 11:15:13 +0200

    Metod för att ändra namn på växt

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -396,6 +396,7 @@ public class Controller {
         if (newName != null && !newName.trim().isEmpty()) {
             currentPlant.setName(newName);
             JOptionPane.showMessageDialog(null, "Plant name changed to: " + currentPlant.getName());
+            view.getCenterPanel().updatePlantName(currentPlant.getPlantName());
         } else {
             JOptionPane.showMessageDialog(null, "Invalid input. Name not changed.");
         }
